8c5901912f6670c2b6e77c24557217d8467184a5
Adewunmi Oladele <87828937+Rachamv@users.noreply.github.com>
2022-11-08 04:20:18 +0000

    update

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 """This is a virtual Coffee machine"""
-# TODO print logo
 from art import logo
 from test import MENU, resources
+from time import sleep
 from replit import clear
 
-print(logo)
+#print(logo)
+
 profit = 0
 
 
@@ -12,59 +13,53 @@ def set_profit(x):
     """To save data on profit"""
     global profit
     profit = profit + x
+    
 
-
-def process_admin():
-    """This function for report and refill the coffee resources """
-    print("Welcome to the Admin panel")
-    check = input("What would you like to do? Report or refill: ").lower()
-    if check == "report":
-        return process_report()
-    elif check == "refill":
-        resources["water"] = 300
-        resources["milk"] = 200
-        resources["coffee"] = 100
-        choice()
-    elif check == "off":
-        exit()
-
-
-#  TODO Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
 def choice():
     """To get user input if they would like to get another cup of coffee"""
-    # clear()
+    print(logo)
     order_again = input("Would you like to order again? Y or N : ").lower()
     if order_again == "y":
         return start()
     elif order_again == "admin":
         return process_admin()
     else:
-        print("Thanks see  you again")
+        print("Invalid Input") 
 
-
-# TODO Print report
-def process_report():
+        
+def process_admin():
+    """This function for report and refill the coffee resources and turn off the machine."""
+    print("Welcome to the Admin panel")
     """Report on resources and profit"""
     print("Current Resources")
     print(f"Water: {resources['water']} ml")
     print(f"Milk: {resources['milk']} ml")
     print(f"Coffee: {resources['coffee']} ml")
     print(f"Money: ${round(profit, 2)}")
-    process_admin()
-
-
-# TODO Make Coffee.
-def process_making(user_choice):
-    """Deduct the required ingredients from the resources and turn off the machine."""
-    if user_choice in MENU:
-        check_resources(user_choice)
-        for item in resources:
-            resources[item] -= MENU[user_choice]["ingredients"][item]
-        process_coins(user_choice)
+    check = input("What would you like to do? Homepage, refill or off: ").lower()
+    if check == "homepage":
+        choice()
+    elif check == "refill":
+        resources["water"] = 300
+        resources["milk"] = 200
+        resources["coffee"] = 100
+        process_admin()
+    elif check == "off":
+        exit()
     else:
-        print("Invalid Input"
-              )  # Turn off the Coffee Machine by entering “off” to the prompt
-        start()
+        print("Invalid Input") 
+        process_admin()
+
+
+def process_making(user_choice, coffee):
+    """Deduct the required ingredients from the resources."""
+    for item in resources:
+        resources[item] -= coffee[item]
+    print(f"Enjoy your {user_choice} ☕")
+    sleep(2)
+    clear()
+    choice()
+
 
 def get_player_int(text):
 
@@ -82,52 +77,61 @@ def get_player_int(text):
   
     except:
       print("I'm terribly sorry, but we require an integer...")
- 
   return user_input
 
-# TODO Process coins.
-def process_coins(user_choice):
+
+def process_coins(coffee):
     """calculate the total money and check for change or refund """
     
     quarters = get_player_int("Please insert quarters: ") * 0.25
     dime = get_player_int("Please insert dimes: ") * 0.1
     nickle = get_player_int("Please insert nickles: ") * 0.05
-    penny = get_player_int("Please insert nickles: ") * 0.01
+    penny = get_player_int("Please insert penny: ") * 0.01
 
     total = (quarters + dime + nickle + penny)
 
-    if total == MENU[user_choice]["cost"]:
-        print(f"Enjoy your {user_choice} ☕")
-        set_profit(MENU[user_choice]['cost'])
-        choice()
-    elif total > MENU[user_choice]["cost"]:
-        charge = total - MENU[user_choice]['cost']
-        print(f"Enjoy your {user_choice} ☕")
+    if total == coffee["cost"]:
+        set_profit(coffee['cost'])
+    elif total > coffee["cost"]:
+        charge = total - coffee['cost']
         print(f"Here is your change ${round(charge, 2)}")
-        set_profit(MENU[user_choice]['cost'])
-        choice()
+        set_profit(coffee['cost'])
     else:
         print(f"Sorry that's not enough money. Money refunded.${total}")
+        sleep(2)
+        clear()
         choice()
-
     return None
 
-# TODO Check resources sufficient?
-def check_resources(user_choice):
+
+def check_resources(coffee):
     """TO check if resources are sufficient."""
     for item in resources:
-        if resources[item] >= MENU[user_choice]["ingredients"][item]:
+        if resources[item] >= coffee[item]:
             return True
         else:
             print(f"Sorry there is not enough {item}.")
+            sleep(2)
+            clear()
             start()
-
-
+            
 def start():
     """To get user choice of coffee """
-    user_choice = input(
-        "What would you like? (espresso/latte/cappuccino): ").lower()
-    process_making(user_choice)
+    print(logo)
+    user_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    access = ["espresso", "latte", "cappuccino", "admin"]
+    if user_choice in access:
+        if user_choice == "admin":
+            return process_admin()
+        else:
+            coffee = MENU[user_choice]
+            if check_resources(coffee["ingredients"]):
+                payment = process_coins(coffee)
+                if payment != 0:
+                    process_making(user_choice, coffee["ingredients"])
+    else:
+        print("Invaild input")
+        start()
 
-
+        
 start()
